f5a519b9a6f3ebb85aa8b6110e89ecd0f86afd1b
Uday032 <rahul.uday07@gmail.com>
2020-12-23 00:22:07 +0530

    Front-end-Setup

--- frontend/src/pages/Location.js
+++ frontend/src/pages/Location.js
@@ -8,20 +8,119 @@ import Col from 'react-bootstrap/Col'
 //Components
 import ReactSelect from '../components/ReactSelect'
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
+//axios api
+import instance from '../axios';
 
 export default class Location extends Component {
 
-    state = {
-        selectedOption: null,
+    constructor(){
+        super();
+        this.state = {
+            selectedstate: '',
+            selecteddistrict: '',
+            selectedsub_district: '',
+            selectedvillage_name: '',
+            showdistrict: 0,
+            showsub_district: 0,
+            showvillage_name: 0,
+            state: [],
+            district: [],
+            sub_district: [],
+            village_name: [],
+            village_coordinates: [],
+            geometrytype: ''
+        };
+    }
+
+    componentDidMount(){
+
+        let stateurl = '/maps/getstate'
+        instance.get(stateurl)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    state: response.data.map(function(states){
+                        return {
+                            value: states,
+                            label: states
+                        }
+                    })
+                })
+            })
+    }
+
+    handlestateChange = selectedstate => {
+        this.setState({ selectedstate });
+        let districturl = "maps/getdistrict?state="+selectedstate.value;
+        // console.log(districturl);
+        instance.get(districturl)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    district: response.data.map(function(states){
+                        return {
+                            value: states,
+                            label: states
+                        }
+                    }),
+                    showdistrict: 1
+                })
+            })
+        // console.log(this.state.showdistrict+" "+selectedstate);
     };
-    handleChange = selectedOption => {
-        this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
+
+    handledistrictChange = selecteddistrict => {
+        this.setState({ selecteddistrict });
+        let subdistricturl = "maps/getsubdistrict?state="+this.state.selectedstate.value+"&district="+selecteddistrict.value;
+        instance.get(subdistricturl)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    sub_district: response.data.map(function(states){
+                        return {
+                            value: states,
+                            label: states
+                        }
+                    }),
+                    showsub_district: 1
+                })
+            })
+        // console.log(`Option selected:`, selecteddistrict);
+    };
+
+    handlesub_districtChange = selectedsub_district => {
+        this.setState({ selectedsub_district });
+        let villagenameurl = "maps/getlocationname?state="+this.state.selectedstate.value+"&district="+this.state.selecteddistrict.value+
+                            "&subdistrict="+selectedsub_district.value;
+        instance.get(villagenameurl)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    village_name: response.data.map(function(states){
+                        return {
+                            value: states,
+                            label: states
+                        }
+                    }),
+                    showvillage_name: 1
+                })
+            })
+        // console.log(`Option selected:`, selectedsub_district);
+    };
+
+    handlevillage_nameChange = selectedvillage_name => {
+        this.setState({ selectedvillage_name });
+        let coordinates = "maps/getcoordinates?state="+this.state.selectedstate.value+"&district="+this.state.selecteddistrict.value+
+                            "&subdistrict="+this.state.selectedsub_district.value+"&location_name="+selectedvillage_name.value;
+        instance.get(coordinates)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    village_coordinates: response.data[0].geometry.coordinates,
+                    geometrytype: response.data[0].geometry.type
+                })
+            })
+        // console.log(`Option selected:`, selectedvillage_name);
     };
 
 
@@ -34,40 +133,40 @@ export default class Location extends Component {
                             <div className="mb-3">
                                 <p>Select State: </p>
                                 <ReactSelect
-                                    selectedOption = {this.state.selectedOption}
-                                    handleChange={this.handleChange}
-                                    options = {options}
+                                    selectedOption = {this.state.selectedstate}
+                                    handleChange={this.handlestateChange}
+                                    options = {this.state.state}
                                 />
                             </div>
-
-                            <div className="mb-3">
+                            
+                            <div className="mb-3" style={{ display: this.state.showdistrict ? "block" : "none" }}>
                                 <p>Select District: </p>
                                 <ReactSelect
-                                    selectedOption = {this.state.selectedOption}
-                                    handleChange={this.handleChange}
-                                    options = {options}
+                                    selectedOption = {this.state.selecteddistrict}
+                                    handleChange={this.handledistrictChange}
+                                    options = {this.state.district}
                                 />
                             </div>
 
-                            <div className="mb-3">
+                            <div className="mb-3" style={{ display: this.state.showsub_district ? "block" : "none" }}>
                                 <p>Select Sub District: </p>
                                 <ReactSelect
-                                    selectedOption = {this.state.selectedOption}
-                                    handleChange={this.handleChange}
-                                    options = {options}
+                                    selectedOption = {this.state.selectedsub_district}
+                                    handleChange={this.handlesub_districtChange}
+                                    options = {this.state.sub_district}
                                 />
                             </div>
 
-                            <div className="mb-3">
+                            <div className="mb-3" style={{ display: this.state.showvillage_name ? "block" : "none" }}>
                                 <p>Select Village Name: </p>
                                 <ReactSelect
-                                    selectedOption = {this.state.selectedOption}
-                                    handleChange={this.handleChange}
-                                    options = {options}
+                                    selectedOption = {this.state.selectedvillage_name}
+                                    handleChange={this.handlevillage_nameChange}
+                                    options = {this.state.village_name}
                                 />
                             </div>
 
-                            
+                            {this.state.geometrytype}
                         </Col>
                         <Col md="8">
                             Map
